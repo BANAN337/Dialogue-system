0fc638504250e4a0817752441baecfa34d10271b
BANAN337 <sasaagudin385@gmail.com>
2026-06-04 23:20:09 +0300

    Experimented with UI builder

--- Assets/Dialogue System/Scripts/Window Elements/NodeRedactor.cs
+++ Assets/Dialogue System/Scripts/Window Elements/NodeRedactor.cs
@@ -1,29 +1,39 @@
+using System;
 using UnityEditor;
-using UnityEditor.Toolbars;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace Window_Elements
+namespace Dialogue_System.Scripts.Window_Elements
 {
     public class NodeRedactor : EditorWindow
     {
-        private EditorToolbarDropdown _createNodes;
+        [SerializeField] private VisualTreeAsset tree;
+        private ToolbarButton _saveButton;
         
         [MenuItem("Window/Dialogue Editor")]
         public static void OpenMainWindow()
         {
-            var mainWindow = GetWindow<NodeRedactor>("Dialogue Editor");
+            var mainWindow = GetWindow<NodeRedactor>();
             mainWindow.titleContent = new GUIContent("Dialogue Graph Editor");
+        }
+
+        private void CreateGUI()
+        {
+            tree.CloneTree(rootVisualElement);
+            _saveButton = rootVisualElement.Q<ToolbarButton>("SaveButton");
+            _saveButton.clicked += () =>
+            {
+                Debug.Log("Save");
+                var path = EditorUtility.SaveFilePanel("Save Dialogue Graph", Application.dataPath, "DialogueGraph", "asset");
+                Debug.Log(path);
+            };
+            
         }
 
         private void OnEnable()
         {
-            CreateToolbar();
-        }
-
-        private void OnGUI()
-        {
+            //CreateToolbar();
         }
 
         private void CreateToolbar()
